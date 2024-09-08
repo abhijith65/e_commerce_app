79f1906c9c36d119645dd20eea375b67fb140b68
abhijith65 <65abhijith@gmail.com>
2024-09-08 13:11:44 +0530

    commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:e_commerce_app/view/splash.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: Splash(),
+    );
+  }
+}
