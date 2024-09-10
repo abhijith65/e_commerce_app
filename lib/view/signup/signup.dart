4a04abf3825fed6fa4e46680c488ca3d93f503eb
abhijith65 <65abhijith@gmail.com>
2024-09-11 00:00:57 +0530

    final

--- lib/view/signup/signup.dart
+++ lib/view/signup/signup.dart
@@ -19,7 +19,7 @@ class _SignupState extends State<Signup> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
